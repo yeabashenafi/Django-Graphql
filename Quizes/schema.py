c0d1e4fa54b09df0b8dbd527ef19794ca22137f2
Yeabsira Ashenafi <ashenafiyeabsira@gmail.com>
2021-08-05 00:51:00 +0300

    added crud o[erations for the category model

--- Quizes/schema.py
+++ Quizes/schema.py
@@ -50,4 +50,51 @@ class Query(graphene.ObjectType):
     # def resolve_quiz(root, info):
     #     return f"This is the first question"
 
-schema = graphene.Schema(query = Query)    
+class createCategoryMutation(graphene.Mutation):
+
+      class Arguments:
+          id = graphene.ID()
+          name = graphene.String(required=True)
+
+      category = graphene.Field(CategoryType)
+
+      @classmethod
+      def mutate(cls,root,info,name):
+          category = Category(name = name)
+          category.save()
+          return createCategoryMutation(category=category)
+
+class updateCategoryMutation(graphene.Mutation):
+
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required = True)
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls,root,info,name,id):
+        category = Category.objects.get(id = id)
+        category.name = name
+        category.save()
+        return updateCategoryMutation(category=category)
+
+class deleteCategoryMutation(graphene.Mutation):
+
+    class Arguments:
+        id = graphene.ID()
+    
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls,root,info,id):
+        category = Category.objects.get(pk = id)
+        category.delete()
+        return
+class Mutation(graphene.ObjectType):
+
+    add_category = createCategoryMutation.Field()
+    update_category = updateCategoryMutation.Field()
+    delete_category = deleteCategoryMutation.Field()
+     
+schema = graphene.Schema(query = Query,mutation = Mutation)    
